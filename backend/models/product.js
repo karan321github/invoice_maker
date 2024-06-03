ebde192d8e0d5a394e496b26802a11f80b876dda
Karan Singh <131857266+karan321github@users.noreply.github.com>
2024-06-03 09:54:28 +0530

    update product modal

--- backend/models/product.js
+++ backend/models/product.js
@@ -4,6 +4,7 @@ const productSchema = mongoose.Schema({
   name: { type: String, required: true },
   quantity: { type: Number, required: true },
   rate: { type: Number, required: true },
+  
 });
 
 const Product = mongoose.model("Product", productSchema);
