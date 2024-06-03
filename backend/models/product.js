c2d5cefdab9f869d6343a3a6d381c311e901e238
KARAN SINGH <131857266+karan321github@users.noreply.github.com>
2024-06-03 10:00:05 +0530

    Update product.js

--- backend/models/product.js
+++ backend/models/product.js
@@ -4,8 +4,6 @@ const productSchema = mongoose.Schema({
   name: { type: String, required: true },
   quantity: { type: Number, required: true },
   rate: { type: Number, required: true },
-  total: { type: Number, required: true },
-  gst: { type: Number, required: true },
 });
 
 const Product = mongoose.model("Product", productSchema);
